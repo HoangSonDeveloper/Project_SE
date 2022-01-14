6222210fa24e08502ab6c445511d601800bd1867
Duc Thang <ndthang19@apcs.fitus.edu.vn>
2022-01-15 00:03:16 +0700

    update api

--- api/models/Product.js
+++ api/models/Product.js
@@ -1,3 +1,20 @@
+// const mongoose = require("mongoose");
+
+// const ProductSchema = new mongoose.Schema(
+//   {
+//     title: { type: String, required: true, unique: true },
+//     desc: { type: String, required: true, },
+//     img: { type: String, required: true },
+//     categories: { type: Array },
+//     size: { type: String },
+//     color: { type: String },
+//     price: { type: Number, required: true },
+    
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("Product", ProductSchema);
 const mongoose = require('mongoose')
 const productSchema = new mongoose.Schema({
     product_id:{
